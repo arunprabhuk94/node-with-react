18da43af6e8e2959830e698f314d04c0875ee7ab
Arun Prabhu K <arunprabhuk94@gmail.com>
2025-06-18 11:44:36 +0000

    feat(server): add passport login

--- index.ts
+++ index.ts
@@ -1,11 +1,35 @@
 import express from "express";
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { keys } from "./config";
+
 const app = express();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("Access Token:", accessToken);
+      console.log("Refresh Token:", refreshToken);
+      console.log("Profile:", profile);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
+
 const port = 5000;
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 const PORT = process.env.PORT || port;
 app.listen(PORT, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
